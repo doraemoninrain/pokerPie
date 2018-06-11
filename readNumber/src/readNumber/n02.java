7eef100a1cca98ba2a42e8fb55cf279ae017f186
user <user@DESKTOP-P502EN5>
2018-06-11 08:54:27 +0800

    2018/06/11

---
+++ readNumber/src/readNumber/n02.java
@@ -0,0 +1,22 @@
+package readNumber;
+
+public class n02 {
+
+	public static void main(String[] args) {
+		System.out.println("您好");
+		
+		
+		
+		
+		int [] a = {1, 2, 3};
+		
+		//a[].length 有中括號 是 javascript
+		if(a.length < 3) {
+			
+		}
+		
+		
+
+	}
+
+}
